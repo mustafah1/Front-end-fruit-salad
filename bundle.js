98d697e7d883ae2f40263362e2a38f914aef2a42
mustafah1 <89341847+mustafah1@users.noreply.github.com>
2023-04-14 11:25:44 +0100

    initial commit of new repo made from CLI

--- bundle.js
+++ bundle.js
@@ -1,21 +1,38 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const fruitForm = document.querySelector("#inputSection form")
 const fruitList = document.querySelector("#fruitSection ul")
+const fruitNutrition = document.querySelector("#nutritionSection ul")
 
 fruitForm.addEventListener("submit", extractFruit)
 function extractFruit(e){
     e.preventDefault()
     addFruit(e.target[0].value)
-    // console.log(e)
-    // console.log(e.target[0].value)
+    fetchFruiteData(e.target[0].value)
     e.target[0].value = ""
 }
 
+function fetchFruiteData (fruit){
+    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+        // .then((resp) => resp.json())
+        .then(processResponse)
+        .then(data => addFruit(data))
+        .catch(e => console.log(e))
+}
 function addFruit(fruit){
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.textContent = fruit.name
     fruitList.appendChild(li)
+    fruitList.addEventListener("click", removeFruit)
+    function removeFruit(e){
+    e.target.remove()
 }
+    // calories += fruit.fruitNutrition.calories
+    // fruitNutrition.textContent = "Total amount fo calories in your fruit salad is " + calories
+
+}
+
+
+  
 
 // addFruit("Mango")
 
--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 const fruitForm = document.querySelector("#inputSection form")
 const fruitList = document.querySelector("#fruitSection ul")
+const fruitNutrition = document.querySelector("#nutritionSection ul")
 
 fruitForm.addEventListener("submit", extractFruit)
 function extractFruit(e){
     e.preventDefault()
     addFruit(e.target[0].value)
-    // console.log(e)
-    // console.log(e.target[0].value)
+    fetchFruiteData(e.target[0].value)
     e.target[0].value = ""
 }
 
+function fetchFruiteData (fruit){
+    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+        // .then((resp) => resp.json())
+        .then(processResponse)
+        .then(data => addFruit(data))
+        .catch(e => console.log(e))
+}
 function addFruit(fruit){
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.textContent = fruit.name
     fruitList.appendChild(li)
+    fruitList.addEventListener("click", removeFruit)
+    function removeFruit(e){
+    e.target.remove()
 }
+    // calories += fruit.fruitNutrition.calories
+    // fruitNutrition.textContent = "Total amount fo calories in your fruit salad is " + calories
+
+}
+
+
+  
 
 // addFruit("Mango")
 
